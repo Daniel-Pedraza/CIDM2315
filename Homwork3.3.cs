49beeb368aa3c56f9d4d37bdfecbf1f64323cfe5
Daniel-Pedraza <123338672+Daniel-Pedraza@users.noreply.github.com>
2023-02-06 23:11:39 -0600

    Add files via upload

---
+++ Homwork3.3.cs
@@ -0,0 +1,20 @@
+﻿namespace Homework3._3;
+
+internal class NewBaseType
+{
+    static void Main(string[] args)
+    {
+        for (int row = 0; row < 5; row++)
+        {
+            for (int col = 0; col < 5; col++)
+            {
+                if (row >= col)
+                
+                    Console.Write('*');
+                
+            }
+            Console.WriteLine("");
+        }
+    }
+}
+
