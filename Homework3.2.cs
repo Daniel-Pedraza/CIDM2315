354dea2d91aa77ccaedf7efd4c66311de7f46d68
Daniel-Pedraza <123338672+Daniel-Pedraza@users.noreply.github.com>
2023-02-06 22:55:52 -0600

    Add files via upload

---
+++ Homework3.2.cs
@@ -0,0 +1,13 @@
+﻿namespace Homework3._2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int row=0; row<5; row++){
+            for(int col = 0; col<5; col++){
+                Console.Write('#');
+            }
+            Console.WriteLine("");
+        }
+    }
+}
