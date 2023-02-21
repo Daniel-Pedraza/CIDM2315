ebf69f79ea0835401ef026856bae6d5e4813198e
Daniel-Pedraza <123338672+Daniel-Pedraza@users.noreply.github.com>
2023-02-20 23:55:06 -0600

    Homework5.1

---
+++ homework5.1.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework6._2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 3, b = 5, c = 7, d = 9;
+        if(3 < 5){
+            Console.WriteLine("The largest number: 5");
+        }
+        if(7 < 9){
+            Console.WriteLine("The largest number: 9");
+        }
+        if(5 < 9){
+            Console.WriteLine("The largest number: 9");
+        }
+    }
+}
