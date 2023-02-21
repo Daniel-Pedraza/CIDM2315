bbd4879307d72dd96e0cac2c791937a420ed2a68
Daniel-Pedraza <123338672+Daniel-Pedraza@users.noreply.github.com>
2023-02-20 23:53:06 -0600

    Homework5

---
+++ homework5.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework6;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 3, b = 5;
+        if(a < b){
+            Console.WriteLine("5");
+        }
+
+    }
+    
+
+    }
+
